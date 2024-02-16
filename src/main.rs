381fbca297f0280ddb535d3610864b505671e68a
Vincenzo Marturano <mart.vivi@hotmail.it>
2024-02-16 18:11:01 +0100

    Added version command.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
+use std::{time::Duration};
+use clap::{Parser, Subcommand,};
+use serialport::SerialPort;
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Specify serial port
+    #[arg(short, long, value_name = "SERIAL", default_value = "/dev/ttyS0")]
+    serial: String,
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// get version of sa818
+    Version {},
+}
+
 fn main() {
-    let ports = serialport::available_ports().expect("No ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
+    let cli = Cli::parse();
+    println!("using serial port:{}",cli.serial);
+    match cli.command {
+        Some(Commands::Version {}) =>{
+            let mut serial_io =open_serial(cli.serial);
+            let result = sa818::get_version(&mut serial_io);
+            println!("version: {}",result.unwrap())
+        },
+        None =>{},
     }
+}
+
+fn open_serial(serial_port: String) -> Box<dyn SerialPort>{
+    let port = serialport::new(serial_port, 9_600)
+    .timeout(Duration::from_millis(10))
+    .data_bits(serialport::DataBits::Eight)
+    .parity(serialport::Parity::None)
+    .stop_bits(serialport::StopBits::One)
+    .open().expect("Failed to open port");
+    return  port;
 }
